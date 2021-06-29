81bf1ca94f80c58f5cec966f9f5383a70389e21a
carlo <mi0772@gmail.com>
2021-06-29 09:47:11 +0200

    minor fix

--- poc-rimborsi-producer/src/main/java/it/almaviva/aci/pocrimborsiproducer/tasks/RimborsoOutboxTask.java
+++ poc-rimborsi-producer/src/main/java/it/almaviva/aci/pocrimborsiproducer/tasks/RimborsoOutboxTask.java
@@ -11,18 +11,18 @@ import org.springframework.stereotype.Component;
 public class RimborsoOutboxTask {
 
     private final RimborsoOutBoxRepository rimborsoOutBoxRepository;
-    private final SendMessageASyncThread sendMessageAndUpdateState;
+    private final SendMessageASyncThread aSyncThread;
 
     @Autowired
-    public RimborsoOutboxTask(RimborsoOutBoxRepository rimborsoOutBoxRepository, SendMessageASyncThread sendMessageAndUpdateState) {
+    public RimborsoOutboxTask(RimborsoOutBoxRepository rimborsoOutBoxRepository, SendMessageASyncThread aSyncThread) {
         this.rimborsoOutBoxRepository = rimborsoOutBoxRepository;
-        this.sendMessageAndUpdateState = sendMessageAndUpdateState;
+        this.aSyncThread = aSyncThread;
     }
 
     @Scheduled(fixedDelayString = "${app.task.delay:5000}")
     public void processOutBox() {
         log.info("query outbox per invio evento a kafka");
-        this.rimborsoOutBoxRepository.findByStato(0).forEach(sendMessageAndUpdateState::sendMessageAndUpdateState);
+        this.rimborsoOutBoxRepository.findByStato(0).forEach(aSyncThread::sendMessageAndUpdateState);
         log.info("fine");
     }
 }
