a47f88e5cdb4f7be6f21d7841d71b452afc96e2f
carlo <mi0772@gmail.com>
2021-06-30 10:34:59 +0200

    initial commit consumer

---
+++ poc-rimborsi-consumer/src/main/java/it/almaviva/aci/pocrimborsiconsumer/PocRimborsiConsumerApplication.java
@@ -0,0 +1,13 @@
+package it.almaviva.aci.pocrimborsiconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocRimborsiConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocRimborsiConsumerApplication.class, args);
+    }
+
+}
---
+++ poc-rimborsi-consumer/src/test/java/it/almaviva/aci/pocrimborsiconsumer/PocRimborsiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.almaviva.aci.pocrimborsiconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocRimborsiConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
