482b93d17e47a1a9368724e8304b350cfbe110ab
carlo <mi0772@gmail.com>
2021-06-30 15:10:47 +0200

    versione finale consumer

--- poc-rimborsi-consumer/src/main/java/it/almaviva/aci/pocrimborsiconsumer/routes/RimborsoRoute.java
+++ poc-rimborsi-consumer/src/main/java/it/almaviva/aci/pocrimborsiconsumer/routes/RimborsoRoute.java
@@ -1,26 +1,25 @@
 package it.almaviva.aci.pocrimborsiconsumer.routes;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.kafka.KafkaComponent;
 import org.apache.camel.component.kafka.KafkaConstants;
 import org.apache.camel.component.kafka.KafkaManualCommit;
 import org.apache.camel.processor.idempotent.kafka.KafkaIdempotentRepository;
-import org.apache.camel.support.SimpleRegistry;
-import org.apache.camel.support.processor.idempotent.MemoryIdempotentRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class RimborsoRoute extends RouteBuilder {
 
     public RimborsoRoute(CamelContext context) {
         super(context);
     }
 
-    @Value("${idemponent-topid}")
+    @Value("${kafka.idemponent-topid}")
     private String idemponentTopic;
 
     @Value("${kafka.server}")
@@ -36,12 +35,9 @@ public class RimborsoRoute extends RouteBuilder {
 
         from("kafka:{{kafka.topic}}?brokers={{kafka.server}}:{{kafka.port}}&groupId={{kafka.channel}}&autoOffsetReset=earliest&autoCommitEnable=false&allowManualCommit=true")
                 .idempotentConsumer(header("kafka.KEY"), kafkaIdempotentRepository)
-                .process(new Processor() {
-                    public void process(Exchange exchange) throws Exception {
-                        System.out.println("Message Body : " + exchange.getIn().getBody());
-                        KafkaManualCommit manual = exchange.getIn().getHeader(KafkaConstants.MANUAL_COMMIT, KafkaManualCommit.class);
-                        manual.commitSync();
-                    }
+                .process(exchange -> {
+                    log.info("Ricevuto messaggio kafka : {}", exchange.getIn().getBody());
+                    exchange.getIn().getHeader(KafkaConstants.MANUAL_COMMIT, KafkaManualCommit.class).commitSync();
                 });
     }
 }
